247ba464fd27541280321a18ec9226cd317f937f
phpr <63541837+pprokopets@users.noreply.github.com>
2021-06-02 17:30:35 +0300

    Create vlan_add_test.py

---
+++ vlan_add_test.py
@@ -0,0 +1,23 @@
+#weird behaviour on python 3.9/osx: it adds vlan, but script fails before end with error 
+#AttributeError: 'xml.etree.ElementTree.Element' object has no attribute 'getchildren' somewhere in ucs-mdk. 
+#and it works without handle.login() - no idea, why :-\
+#needs an investigation.
+
+from ucsmsdk.ucshandle import UcsHandle
+from ucsmsdk.mometa.fabric.FabricVlan import FabricVlan
+
+#init handle
+handle = UcsHandle("172.28.150.15","ucspe","ucspe",secure=False)
+
+#login
+#handle.login()
+
+mo = FabricVlan(parent_mo_or_dn="fabric/lan",sharing="none",name="vlan22",id="22")
+handle.add_mo(mo)
+handle.commit()
+
+#login
+#handle.login()
+print("vlan added")
+#logout
+handle.logout()
